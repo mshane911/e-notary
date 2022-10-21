c5e4a0dc3b153e88ec827402a9fcc796543ed7e7
Michael Shane <msha0080@student.monash.edu>
2022-10-21 09:25:32 +0700

    initialize client and server code

---
+++ e-notary-server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'e-notary' });
+});
+
+module.exports = router;
