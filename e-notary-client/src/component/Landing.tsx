5d25290218a6399d15c926d39cae1a1af820e1f8
Felicia Winna <winna.felicia@gmail.com>
2022-11-06 00:31:50 +0700

    Added sign in form and fixed responsiveness

--- e-notary-client/src/component/Landing.tsx
+++ e-notary-client/src/component/Landing.tsx
@@ -7,19 +7,39 @@ import '../styles/landing.css'
 export default function Landing() {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
-    const [userType, setUserType] = useState("employee"); // TODO: BALIKIN JADI EMPTY STRING KALO UDAH DI BIKIN FORM NYA
+    const [userType, setUserType] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
 
     const openSignUpForm = () => {
-        document.getElementById("signUp").style.display = "inline-block"
+        document.getElementById("signIn").style.display = "none"
+        document.getElementById("signUp").style.display = "flex"
     }
 
     const closeSignUpForm = () => {
         document.getElementById("signUp").style.display = "none"
     }
 
+    const openSignInForm = () => {
+        document.getElementById("signUp").style.display = "none"
+        document.getElementById("signIn").style.display = "flex"
+    }
+
+    const closeSignInForm = () => {
+        document.getElementById("signIn").style.display = "none"
+    }
+
+    function checkPassword(){
+        return password == passwordConfirm ? true : false
+    }
+
+    // TODO: add check password the same or not
     const submitForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent> | React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+        const isPasswordSame = checkPassword();
+        if (!isPasswordSame) {
+            e.preventDefault();
+        }
+
         const config = {
             method: "POST",
             url: "/api/register/",
@@ -52,42 +72,82 @@ export default function Landing() {
             <img src={require('../media/landing-img.jpg')} className='bgImg' />
             <div className='overlay'></div>
 
-            <div className='visibleContainer' style={{ margin: "100px" }}>
-                <img src={require('../media/logo.png')} className='logo' />
-                <h1 className='sectionTitle'>Meet <span className='appleFont'>E-notary</span></h1>
-                <p className='sectionSubtitle'>Making digital signatures secure and easy</p>
-                <button className='signUpBtn' onClick={openSignUpForm}>SIGN UP NOW</button>
-                <p className='signIn'>Have an account? <span className='underlineText'>Sign In</span></p>
+            <div className='visibleContainer'>
+                <div className='visibleContainerContent'>
+                    <img src={require('../media/logo.png')} className='logo' />
+                    <h1 className='sectionTitle'>Meet <span className='appleFont'>E-notary</span></h1>
+                    <p className='sectionSubtitle'>Making digital signatures secure and easy</p>
+                    <button className='signUpBtn' onClick={openSignUpForm}>SIGN UP NOW</button>
+                    <p className='signInText'>Have an account? <span className='underlineText' onClick={openSignInForm}>Sign In</span></p>
+                </div>
             </div>
 
-            <div className='signUpForm toggleContainer' id='signUp'>
-                <form>
-                    <FontAwesomeIcon icon={faX} className="closeBtn" onClick={closeSignUpForm} />
-                    <h2 className='formTitle'>Sign Up</h2>
-                    <div className='inputContainer'>
-                        <FontAwesomeIcon icon={faUser} />
-                        <input type="text" className="textInput" placeholder="Full Name" name="name" onChange={(e) => setName(e.target.value)} required></input>
-                    </div>
-                    <div className='inputContainer'>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                        <input type="text" className="textInput" placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)} required></input>
-                    </div>
-                    <div className='inputContainer'>
-                        <FontAwesomeIcon icon={faLock} />
-                        <input type="text" className="textInput" placeholder="Password" name="pass" onChange={(e) => setPassword(e.target.value)} required></input>
-                    </div>
+            <div className='toggleContainer'>
+                <div className='formContainer' id='signUp'>
+                    <form>
+                        <FontAwesomeIcon icon={faX} className="closeBtn" onClick={closeSignUpForm} />
+                        <h2 className='formTitle'>Create An Account</h2>
+                        <p className='formSubtitle'>Welcome to E-Notary, Please fill in your details below</p>
+                        <div className='formContent'>
+                            <div className='inputContainer'>
+                                <FontAwesomeIcon icon={faUser} />
+                                <input type="text" className="textInput" placeholder="Full Name" name="name" onChange={(e) => setName(e.target.value)} required></input>
+                            </div>
+                            <div className='inputContainer'>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                                <input type="text" className="textInput" placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)} required></input>
+                            </div>
+                            <div className='inputContainer'>
+                                <FontAwesomeIcon icon={faLock} />
+                                <input type="password"  autoComplete='on' className="textInput" placeholder="Password" name="pass" onChange={(e) => setPassword(e.target.value)} required></input>
+                            </div>
+                            <div className='inputContainer'>
+                                <FontAwesomeIcon icon={faLock} />
+                                <input type="password" autoComplete='on' className="textInput" placeholder="Confirm Your Password" name="passconfirm" onChange={(e) => { setPasswordConfirm(e.target.value)}} required></input>
+                            </div>
+                            <div className="inputWrapper">
+                                <select className='selectWrapper' onChange={(e) => setUserType(e.target.value)}>
+                                    <option defaultValue="" disabled >Select Account Type</option>
+                                    <option value="employee">Employee</option>
+                                    <option value="company">Company</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className='tncLabel'>
+                            <input type="checkbox" name="tnc" value="tnc" required />
+                            <label>I have read and agree to the <br></br><span className='tnc'>Terms & Conditions</span></label>
+                        </div>
 
-                    <div className='tncLabel'>
-                        <input type="checkbox" name="tnc" value="tnc" required />
-                        <label>I have read and agree to the <span className='tnc'>Terms & Conditions</span></label>
-                    </div>
+                        <div className='submitWrapper'>
+                            <input type="submit" value="Create Account" className='signUpSubmit' onClick={submitForm}></input>
+                        </div>
+                        
+                    </form>
+                </div>
 
-                    <div className='submitWrapper'>
-                        <input type="submit" value="Create Account" className='signUpSubmit' onClick={submitForm}></input>
-                    </div>
-                </form>
+                <div className='formContainer' id='signIn'>
+                    <form>
+                        <FontAwesomeIcon icon={faX} className="closeBtn" onClick={closeSignInForm} />
+                        <h2 className='formTitle'>Welcome Back</h2>
+                        <p className='formSubtitle'>Please Fill in Your Details </p>
+                        <div className='formContent'>
+                            <div className='inputContainer'>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                                <input type="text" className="textInput" placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)} required></input>
+                            </div>
+                            <div className='inputContainer'>
+                                <FontAwesomeIcon icon={faLock} />
+                                <input type="password" autoComplete='on' className="textInput" placeholder="Password" name="pass" onChange={(e) => setPassword(e.target.value)} required></input>
+                            </div>
+                        </div>
+
+                        <div className='submitWrapper'>
+                            <input type="submit" value="Sign In" className='signInSubmit'></input>
+                        </div>
+                        
+                    </form>
+                </div>
             </div>
-
         </div>
     )
 }
